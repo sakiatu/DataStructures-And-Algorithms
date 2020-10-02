c326a521f99164d61526e143b958e0ad28caa98b
sakib20 <sakib20@iut-dhaka.edu>
2020-10-02 11:32:03 +0600

    solve task2_1

---
+++ LAB_WORKS/LAB_1/task2_1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void sort(int *arr, int n) {
+    for (int i = 1; i < n; i++) {
+        int value = arr[i];
+        int hole = i;
+        while (hole > 0 && value < arr[hole - 1]) {
+            arr[hole] = arr[hole - 1];
+            hole--;
+        }
+        arr[hole] = value;
+    }
+}
+
+void printMedian(int *arr, int size) {
+    int median = size % 2 ? arr[size / 2] : (arr[size / 2] + arr[size / 2 - 1]) / 2;
+    printf("%d\n", median);
+}
+
+int main() {
+    int arr[1000];
+    int i = 0;
+    int j = 0;
+    while (++i) {
+        scanf("%d", &arr[j++]);
+        sort(arr, i);//i = size of current arr
+        printMedian(arr, i);
+    }
+}
