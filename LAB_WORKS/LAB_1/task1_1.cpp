c81cc8f275d27d339fc7f45cc6cf38f635acbb98
sakib20 <sakib20@iut-dhaka.edu>
2020-10-04 12:03:11 +0600

    update task1_1 - O(n^2)

--- LAB_WORKS/LAB_1/task1_1.cpp
+++ LAB_WORKS/LAB_1/task1_1.cpp
@@ -2,33 +2,41 @@
 
 using namespace std;
 
-void sort(vector<pair<int, int>> &vec, int n) {
+void sort(int *arr, int n) {
     for (int i = 1; i < n; i++) {
-        int val1 = vec[i].first;
-        int val2 = vec[i].second;
+        int value = arr[i];
         int hole = i;
-        while (hole > 0 && val1 <= vec[hole - 1].first && val2 <= vec[hole - 1].second) {
-            vec[hole].first = vec[hole - 1].first;
-            vec[hole].second = vec[hole - 1].second;
+        while (hole > 0 && value < arr[hole - 1]) {
+            arr[hole] = arr[hole - 1];
             hole--;
         }
-        vec[hole].first = val1;
-        vec[hole].second = val2;
+        arr[hole] = value;
     }
 }
+
+void printNthPair(int *arr, int n, int k, map<int, int> count, map<int, int> firstIndex) {
+    int i = firstIndex[arr[k / n]];
+    int j = (k - n * i) / count[arr[i]];
+
+    printf("%d %d\n", arr[i], arr[j]);
+}
+
 
 int main() {
     int n, k;
     scanf("%d%d", &n, &k);
     int arr[n];
+    map<int, int> count, firstIndex;
+
     for (int i = 0; i < n; ++i)
         scanf("%d", &arr[i]);
 
-    vector<pair<int, int>> vec;
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            vec.push_back(make_pair(arr[i], arr[j]));
-    sort(vec, n * n);
+    sort(arr, n);
 
-    printf("%d %d\n", vec[k - 1].first, vec[k - 1].second);
+    for (int i = 0; i < n; ++i) {
+        count[arr[i]] += 1;
+        if (count[arr[i]] == 1) firstIndex[arr[i]] = i;
+    }
+
+    printNthPair(arr, n, k - 1, count, firstIndex);
 }
